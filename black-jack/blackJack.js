254e488fead9202a8335ff11986a9016a2ea0fec
Yu Wei Koh <kohyu@oregonstate.edu>
2020-12-03 13:41:26 +0800

    added event listeners

--- black-jack/blackJack.js
+++ black-jack/blackJack.js
@@ -101,7 +101,96 @@ function dealerBot(){
  * init()					- adds 6 decks and deal cards()
  * dealCards()				- clear hands then deal cards to player and dealer
  * dealHand(hand)				- deal 1 card to selected hand
- * count(hand)				- returns total count of hand
+ * dealCards()				- returns total count of hand
  * generateDecks(n) 		- adds n decks onto deck, default n = 1
  * compHand(player, dealer)	- return -1, 0, 1 as player win, tie, dealer win respectively
  */
+
+
+
+
+
+
+
+
+
+
+
+ /**********************************************************************
+  * EVENT LISTENERS 
+  **********************************************************************/
+
+
+
+var chip5 = document.getElementById('5');
+var chip10 = document.getElementById('10');
+var chip50 = document.getElementById('50');
+var chip100 = document.getElementById('100');
+var chip500 = document.getElementById('500');
+var chip1000 = document.getElementById('1000');
+var chip_container = document.getElementById('chip-container');
+
+
+chip5.addEventListener('click', function(){
+	console.log("==chip5 was clicked");
+});
+
+chip10.addEventListener('click', function(){
+	console.log("==chip10 was clicked");
+});
+
+chip50.addEventListener('click', function(){
+	console.log("==chip50 was clicked");
+});
+
+chip100.addEventListener('click', function(){
+	console.log("==chip100 was clicked");
+});
+
+chip500.addEventListener('click', function(){
+	console.log("==chip500 was clicked");
+});
+
+chip1000.addEventListener('click', function(){
+	console.log("==chip1000 was clicked");
+});
+
+
+
+var bet = document.getElementById('bet-button');
+var hit = document.getElementById('hit-button');
+var stand = document.getElementById('stand-button');
+var split = document.getElementById('split-button');
+var start = document.getElementById('start-button');
+var next = document.getElementById('next-hand-button')
+
+
+bet.addEventListener('click', function(){
+	console.log("==bet was clicked");
+});
+
+hit.addEventListener('click', function(){
+	console.log("==hit was clicked");
+});
+
+stand.addEventListener('click', function(){
+	console.log("==stand was clicked");
+});
+
+split.addEventListener('click', function(){
+	console.log("==split was clicked");
+});
+
+next.addEventListener('click', function(){
+	console.log("==next was clicked");
+});
+
+start.addEventListener('click', function(){
+	console.log("==start was clicked");
+	// if (chip_container.style.visibility == 'hidden'){
+	chip_container.style.visibility = 'visible';
+	start.style.visibility = 'hidden';
+	bet.style.visibility = 'visible';
+	init();
+	// }
+});
